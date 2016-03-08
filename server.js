99d2f0b2902d441b148f62898379924703c97486
Simran Preet Singh <simran5590@gmail.com>
2016-03-07 22:35:19 -0800

    Get all todos

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: 'Meet mom for lunch',
+  completed: false
+}, {
+  id: 2,
+  description: 'Go to Market',
+  completed: false
+}, {
+  id: 3,
+  description: 'Go to Office',
+  completed: true
+}];
 
 app.get('/', (request, response) => {
   response.send('Todo API Route');
+});
+
+//GET all todos
+app.get('/todos', (request, response) => {
+  response.json(todos);
+});
+
+// GET a particular todo/:id
+app.get('/todos/:id', (request, response) => {
+
+  var todoid = request.params.id;
+
+  todos.forEach(function(todo) {
+    if(todo.id ==  todoid) {
+      response.json(todo);
+    } else {
+      response.status(404).send();
+    }
+  });
 });
 
 app.listen(PORT, () => {
