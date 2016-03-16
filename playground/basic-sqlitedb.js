0d31dc6a78527e0c3f89f3a9caba49e7daaa031a
Simran Preet Singh <simran5590@gmail.com>
2016-03-15 22:03:09 -0700

    Inserting Data and searching findAll

--- playground/basic-sqlitedb.js
+++ playground/basic-sqlitedb.js
@@ -6,13 +6,49 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
   description: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1,500]
+    }
   },
   completed: {
-    type: Sequelize.BOOLEAN
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
   }
 });
 
-sequelize.sync().then(function() {
+// sequelize.sync({force: true}).then(function()    is used to drop the table first.
+sequelize.sync({force: true}).then(function() {
   console.log('Everything is synced');
+  // Putting values in the dabase
+  Todo.create({
+    description: 'Take out Trash',
+  }).then(function(todo) {
+    return Todo.create({
+      description: "Clean Office"
+    })
+  }).then(function(todo) {
+    //return Todo.findById(1);
+    return Todo.findAll({
+      where: {
+        completed: false,
+        description: {
+          $like: '%o%'
+        }
+      }
+    });
+  }).then(function(todos) {
+    if(todos) {
+      // console.log(todos);
+      todos.forEach(function(todo) {
+        console.log(todo.toJSON());
+      });
+    } else {
+      console.log('No todo found');
+    }
+  }).catch(function(e) {
+    console.log(e);
+  });
 });
