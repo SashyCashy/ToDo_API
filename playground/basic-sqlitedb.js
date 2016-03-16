7cca2752b8a0a9a8f39fea6b50ecb7687c2126aa
Simran Preet Singh <simran5590@gmail.com>
2016-03-15 22:11:55 -0700

    Challange done

--- playground/basic-sqlitedb.js
+++ playground/basic-sqlitedb.js
@@ -22,6 +22,40 @@ var Todo = sequelize.define('todo', {
 // sequelize.sync({force: true}).then(function()    is used to drop the table first.
 sequelize.sync({force: true}).then(function() {
   console.log('Everything is synced');
+
+  Todo.create({
+    description: 'Get the tasks done.'
+  }).then(function(todo) {
+    return Todo.create({
+      description: 'Get Milk from store'
+    });
+  }).then(function(todo) {
+    return Todo.create({
+      description: 'Start Learning NodeJS',
+      completed: true
+    });
+  }).then(function() {
+    return Todo.findAll({
+      where: {
+        description: {
+          $like: '%Something%'
+        }
+      }
+    });
+  }).then(function(todos) {
+    if(todos) {
+      todos.forEach(function(todo) {
+        console.log(todo.toJSON());
+      });
+    } else {
+      console.log('No matching todos found');
+    }
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+
+  /*
   // Putting values in the dabase
   Todo.create({
     description: 'Take out Trash',
@@ -51,4 +85,5 @@ sequelize.sync({force: true}).then(function() {
   }).catch(function(e) {
     console.log(e);
   });
+  */
 });
